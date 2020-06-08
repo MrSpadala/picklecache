1d4f838c1e7a4a8060d3e5683dc0de8d60e11f85
spadala macbook <asd>
2020-06-08 16:40:51 +0200

    add arg_check optional kwarg, improve docs

--- pklcache/__init__.py
+++ pklcache/__init__.py
@@ -1,20 +1,20 @@
 """
 Function decorator caching on disk the result of a function call.
 
-If the cache file 'fpath' is not found, then the decorated function 
-is called and its result is saved with pickle on 'fpath', else
-the file is loaded and returned, and the function is not executed
+The decorated function is executed and its result is stored on disk.
+Later invocations of the function won't execute it, but just load
+its result from disk.
 
 Example:
 
-@cache("myfile.pickle")
+@cache("foo-result.pickle")
 def foo(args):
     ...
     return result
     
 """
 
-__version__ = "0.1"
+__version__ = "0.2"
 __author__ = "Pietro Spadaccino"
 
 
@@ -23,12 +23,25 @@ import os
 import pickle
 import functools
 
+MAGIC = "PKLCACHE-whocaresabouttypes"
 
-def cache(fpath, enabled=True):
+def cache(fpath, arg_check=False, enabled=True):
     """
     Decorator caching on file `fpath` the result of a function using pickle.
-    When `enabled` is False the caching has no effect and the function
-    is executed normally. 
+
+    Args:
+        enabled (bool): if False the caching has no effect and the function
+            is executed normally. Defaults to True.
+        arg_check (bool): If True, when loading the cached result arguments
+            passed to the function that produced the cached result are compared
+            to arguments of the current inocation. If they are not the same
+            then the cached result is not loaded and the function is executed.
+
+    Example:
+        @cache("foo-result.pickle")
+        def foo(args):
+            ...
+            return result
     """
 
     def decorator(func):
@@ -39,15 +52,37 @@ def cache(fpath, enabled=True):
             if not enabled:
                 return func(*args, **kwargs)
 
-            # If file is present load it and return its contents
+            # Check if cache file is present. If yes load its contents
             if os.path.isfile(fpath):
-                print({func.__name__}, " result loaded from disk")
-                return pickle.load(open(fpath, "rb"))
+                cached = pickle.load(open(fpath, "rb"))
+                
+                # If arg_check is requested check the arguments.
+                # When the check is requested, the cached result is 
+                # stored on disk with a tuple of 4 elements:
+                # (`result`, args, kwargs, MAGIC)
+                # where `result` is the cached function result, args
+                # and kwargs are the argument of the function producing
+                # `result`.
+                is_cached_tuple = (hasattr(cached, '__len__') and len(cached) == 4 and
+                        cached[3] == MAGIC)
+                if not arg_check or (is_cached_tuple and cached[1] == args and
+                        cached[2] == kwargs):
+                    
+                    print("'{}' not executed, result loaded from disk".format(func.__name__))
+                    return cached[0] if is_cached_tuple else cached
 
-            # Cache file not present, so execute the function and dump
-            # the result on the file. Return then the result
+
+            # Cache file not present or argcheck failed. Execute the
+            # function and dump the result on the file. Return then the result
             result = func(*args, **kwargs)
-            pickle.dump(result, open(fpath, "wb"))
+            to_cache = result if not arg_check else (result, args, kwargs, MAGIC)
+            try:
+                pickle.dump(to_cache, open(fpath, "wb"))
+            except Exception as e:
+                print("Exception", e.__class__, "occurred while caching",func.__name__ ,"result")
+                print("Trying to not save arguments...")
+                pickle.dump(result, open(fpath, "wb"))
+
             return result
 
         return wrapper
